2e10687332f18f18ac46c78704cc878f73ff68f5
shmeckley <67341844+shmeckley@users.noreply.github.com>
2020-06-24 10:49:15 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,172 +1,25 @@
-function Game () {
-    basic.clearScreen()
-    game.setScore(0)
-    hero = game.createSprite(2, 4)
-    Catch = game.createSprite(randint(1, 3), 0)
-    for (let index = 0; index < 21; index++) {
-        basic.pause(500)
-        Catch.change(LedSpriteProperty.Y, 1)
-        if (Catch.isTouching(hero)) {
-            game.addScore(1)
-            Catch.delete()
-            Catch = game.createSprite(randint(1, 3), 0)
-        } else {
-            if (Catch.isTouchingEdge()) {
-                basic.pause(100)
-                game.gameOver()
-            }
-        }
-    }
-    images.createBigImage(`
-        . # . . . # . . . .
-        . # # . . # # . . .
-        . # # # . # # # . .
-        . # # . . # # . . .
-        . # . . . # . . . .
-        `).scrollImage(1, 200)
-    for (let index = 0; index < 28; index++) {
-        basic.pause(400)
-        Catch.change(LedSpriteProperty.Y, 1)
-        if (Catch.isTouching(hero)) {
-            game.addScore(1)
-            Catch.delete()
-            Catch = game.createSprite(randint(1, 3), 0)
-        } else {
-            if (Catch.isTouchingEdge()) {
-                basic.pause(100)
-                game.gameOver()
-            }
-        }
-    }
-    images.createBigImage(`
-        . # . . . # . . . .
-        . # # . . # # . . .
-        . # # # . # # # . .
-        . # # . . # # . . .
-        . # . . . # . . . .
-        `).scrollImage(1, 200)
-    for (let index = 0; index < 40; index++) {
-        basic.pause(300)
-        Catch.change(LedSpriteProperty.Y, 1)
-        if (Catch.isTouching(hero)) {
-            game.addScore(1)
-            Catch.delete()
-            Catch = game.createSprite(randint(1, 3), 0)
-        } else {
-            if (Catch.isTouchingEdge()) {
-                basic.pause(100)
-                game.gameOver()
-            }
-        }
-    }
-    images.createBigImage(`
-        . # . . . # . . . .
-        . # # . . # # . . .
-        . # # # . # # # . .
-        . # # . . # # . . .
-        . # . . . # . . . .
-        `).scrollImage(1, 200)
-    for (let index = 0; index < 80; index++) {
-        basic.pause(250)
-        Catch.change(LedSpriteProperty.Y, 1)
-        if (Catch.isTouching(hero)) {
-            game.addScore(1)
-            Catch.delete()
-            Catch = game.createSprite(randint(1, 3), 0)
-        } else {
-            if (Catch.isTouchingEdge()) {
-                basic.pause(100)
-                game.gameOver()
-            }
-        }
-    }
-    images.createBigImage(`
-        . # . . . # . . . .
-        . # # . . # # . . .
-        . # # # . # # # . .
-        . # # . . # # . . .
-        . # . . . # . . . .
-        `).scrollImage(1, 200)
-    for (let index = 0; index < 80; index++) {
-        basic.pause(200)
-        Catch.change(LedSpriteProperty.Y, 1)
-        if (Catch.isTouching(hero)) {
-            game.addScore(1)
-            Catch.delete()
-            Catch = game.createSprite(randint(1, 3), 0)
-        } else {
-            if (Catch.isTouchingEdge()) {
-                basic.pause(100)
-                game.gameOver()
-            }
-        }
-    }
-    images.createBigImage(`
-        . # . . . # . . . .
-        . # # . . # # . . .
-        . # # # . # # # . .
-        . # # . . # # . . .
-        . # . . . # . . . .
-        `).scrollImage(1, 200)
-    for (let index = 0; index < 120; index++) {
-        basic.pause(150)
-        Catch.change(LedSpriteProperty.Y, 1)
-        if (Catch.isTouching(hero)) {
-            game.addScore(1)
-            Catch.delete()
-            Catch = game.createSprite(randint(1, 3), 0)
-        } else {
-            if (Catch.isTouchingEdge()) {
-                basic.pause(100)
-                game.gameOver()
-            }
-        }
-    }
-    images.createBigImage(`
-        . # . . . # . . . .
-        . # # . . # # . . .
-        . # # # . # # # . .
-        . # # . . # # . . .
-        . # . . . # . . . .
-        `).scrollImage(1, 200)
-    for (let index = 0; index < 1e+37; index++) {
-        basic.pause(100)
-        Catch.change(LedSpriteProperty.Y, 1)
-        if (Catch.isTouching(hero)) {
-            game.addScore(1)
-            Catch.delete()
-            Catch = game.createSprite(randint(1, 3), 0)
-        } else {
-            if (Catch.isTouchingEdge()) {
-                basic.pause(100)
-                game.gameOver()
-            }
-        }
-    }
-    basic.showString("Uhhhhhhhhh... you win?")
-    game.gameOver()
-}
 input.onButtonPressed(Button.A, function () {
-    control.raiseEvent(
-    EventBusSource.MICROBIT_ID_BUTTON_A,
-    EventBusValue.MICROBIT_BUTTON_EVT_DOWN
-    )
-    if (gamez == 1) {
-        hero.change(LedSpriteProperty.X, -1)
+    if (gamez == 2) {
+        hard.change(LedSpriteProperty.X, -1)
     }
 })
 input.onGesture(Gesture.LogoUp, function () {
     if (gamez == 2) {
         hard.change(LedSpriteProperty.Y, 1)
     }
+    basic.pause(300)
 })
 function end () {
     game.setScore(scre)
+    basic.showString("YOU WIN")
     game.gameOver()
 }
 input.onGesture(Gesture.TiltLeft, function () {
-    if (gamez == 2) {
-        hard.change(LedSpriteProperty.X, -1)
+    while (input.isGesture(Gesture.TiltLeft)) {
+        if (gamez == 2) {
+            hard.change(LedSpriteProperty.X, -1)
+        }
+        basic.pause(300)
     }
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
@@ -174,75 +27,32 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTT
     	
     } else {
         basic.pause(100)
-        gamez = 2
         basic.pause(100)
-        if (gamez == 2) {
-            hardest_game()
-        }
+        hardest_game()
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        . . # . .
-        # # # # #
-        . . # . .
-        . # . # .
-        . # . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        # # # # #
-        . # # . .
-        . # . # .
-        . # . # .
-        `)
-    basic.showLeds(`
-        # . . # #
-        # . # . .
-        # # # # .
-        . # . . .
-        # . # . .
-        `)
-    basic.showString("Game made by Ethan    Instructions: use a and b to navigate your character on the bottom of the screen under the falling balls...       press a+b to start                         you still here?.........  ")
-    basic.showLeds(`
-        . . # . .
-        # # # # #
-        . . # . .
-        . # . # .
-        . # . # .
-        `)
-    basic.showLeds(`
-        . # . . .
-        # # # # #
-        . # # . .
-        . # . # .
-        . # . # .
-        `)
-    basic.showLeds(`
-        # . . # #
-        # . # . .
-        # # # # .
-        . # . . .
-        # . # . .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    control.raiseEvent(
-    EventBusSource.MICROBIT_ID_BUTTON_B,
-    EventBusValue.MICROBIT_EVT_ANY
-    )
-    if (gamez == 1) {
-        hero.change(LedSpriteProperty.X, 1)
+    if (gamez == 2) {
+        hard.change(LedSpriteProperty.Y, -1)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    if (gamez == 2) {
+        hard.change(LedSpriteProperty.X, 1)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    gamez = 2
+})
 function hardest_game () {
-    scre = 3000
+    scre = 300
     hard = game.createSprite(0, 4)
     enemy = game.createSprite(3, 4)
     enemy_2 = game.createSprite(1, 0)
     goal = game.createSprite(4, 2)
     enemy_2.turn(Direction.Left, 90)
     enemy.turn(Direction.Right, 90)
+    gamez = 2
     for (let index = 0; index < 99999999999; index++) {
         enemy.turn(Direction.Right, 180)
         enemy_2.turn(Direction.Right, 180)
@@ -374,6 +184,9 @@ function hardest_game () {
             _3.move(1)
             _4.move(1)
             _5.move(1)
+            if (hard.isTouching(goal)) {
+                break;
+            }
             if (hard.isTouching(_1)) {
                 game.setScore(0)
                 game.gameOver()
@@ -398,9 +211,6 @@ function hardest_game () {
                 goal = game.createSprite(4, 0)
             } else {
                 goal.delete()
-            }
-            if (hard.isTouching(goal)) {
-                break;
             }
         }
         if (hard.isTouching(goal)) {
@@ -447,6 +257,9 @@ function hardest_game () {
             _3.move(1)
             _4.move(1)
             _5.move(1)
+            if (hard.isTouching(goal)) {
+                break;
+            }
             if (hard.isTouching(_1)) {
                 game.setScore(0)
                 game.gameOver()
@@ -484,9 +297,6 @@ function hardest_game () {
             } else {
                 goal.delete()
             }
-            if (hard.isTouching(goal)) {
-                break;
-            }
         }
         if (hard.isTouching(goal)) {
             break;
@@ -501,41 +311,156 @@ function hardest_game () {
     _4.delete()
     basic.pause(100)
     _5.delete()
+    basic.pause(100)
+    _6.delete()
+    basic.pause(100)
+    _7.delete()
+    basic.pause(100)
+    _8.delete()
+    basic.pause(100)
     hard.delete()
     goal.delete()
-    basic.showString("YOU WIN")
+    enemy = game.createSprite(3, 0)
+    goal = game.createSprite(3, 0)
+    _1 = game.createSprite(0, 0)
+    _2 = game.createSprite(2, 1)
+    _3 = game.createSprite(3, 1)
+    _4 = game.createSprite(4, 0)
+    _5 = game.createSprite(0, 2)
+    hard = game.createSprite(0, 4)
+    _6 = game.createSprite(1, 3)
+    _7 = game.createSprite(2, 3)
+    _8 = game.createSprite(3, 3)
+    for (let index = 0; index < 999999999999999; index++) {
+        basic.pause(300)
+        scre += -1
+        if (m == 13) {
+            enemy.set(LedSpriteProperty.X, 3)
+            enemy.set(LedSpriteProperty.Y, 0)
+            m = 0
+        }
+        if (m == 12) {
+            enemy.change(LedSpriteProperty.X, -1)
+            m = 13
+        }
+        if (m == 11) {
+            enemy.change(LedSpriteProperty.X, -1)
+            m = 12
+        }
+        if (m == 10) {
+            enemy.change(LedSpriteProperty.X, -1)
+            m = 11
+        }
+        if (m == 9) {
+            enemy.change(LedSpriteProperty.X, -1)
+            m = 10
+        }
+        if (m == 8) {
+            enemy.change(LedSpriteProperty.Y, 1)
+            m = 9
+        }
+        if (m == 7) {
+            enemy.change(LedSpriteProperty.Y, 1)
+            m = 8
+        }
+        if (m == 6) {
+            enemy.change(LedSpriteProperty.X, 1)
+            m = 7
+        }
+        if (m == 5) {
+            enemy.change(LedSpriteProperty.X, 1)
+            m = 6
+        }
+        if (m == 4) {
+            enemy.change(LedSpriteProperty.X, 1)
+            m = 5
+        }
+        if (m == 3) {
+            enemy.change(LedSpriteProperty.Y, 1)
+            m = 4
+        }
+        if (m == 2) {
+            enemy.change(LedSpriteProperty.Y, 1)
+            m = 3
+        }
+        if (m == 1) {
+            enemy.change(LedSpriteProperty.X, -1)
+            m = 2
+        }
+        if (m == 0) {
+            enemy.change(LedSpriteProperty.X, -1)
+            m = 1
+        }
+        if (hard.isTouching(_1)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (hard.isTouching(_2)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (hard.isTouching(_3)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (hard.isTouching(_4)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (hard.isTouching(_5)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (hard.isTouching(_6)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (hard.isTouching(_7)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (hard.isTouching(enemy)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (hard.isTouching(_8)) {
+            game.setScore(0)
+            game.gameOver()
+        }
+        if (goal.isDeleted()) {
+            goal = game.createSprite(3, 0)
+        } else {
+            goal.delete()
+        }
+        if (hard.isTouching(goal)) {
+            break;
+        }
+    }
     end()
 }
 input.onGesture(Gesture.TiltRight, function () {
-    if (gamez == 2) {
-        hard.change(LedSpriteProperty.X, 1)
+    while (input.isGesture(Gesture.TiltRight)) {
+        if (gamez == 2) {
+            hard.change(LedSpriteProperty.X, 1)
+        }
+        basic.pause(300)
     }
 })
 input.onGesture(Gesture.LogoDown, function () {
-    if (gamez == 2) {
-        hard.change(LedSpriteProperty.Y, -1)
-    }
-})
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_EVT_ANY, function () {
-    if (gamez == 2) {
-    	
-    } else {
-        basic.pause(100)
-        gamez = 1
-        basic.pause(100)
-        if (gamez == 1) {
-            Game()
+    while (input.isGesture(Gesture.LogoDown)) {
+        if (gamez == 2) {
+            hard.change(LedSpriteProperty.Y, -1)
         }
+        basic.pause(300)
     }
 })
+let m = 0
 let goal: game.LedSprite = null
 let enemy_2: game.LedSprite = null
 let enemy: game.LedSprite = null
 let scre = 0
 let hard: game.LedSprite = null
 let gamez = 0
-let Catch: game.LedSprite = null
-let hero: game.LedSprite = null
 let _8: game.LedSprite = null
 let _7: game.LedSprite = null
 let _6: game.LedSprite = null
@@ -544,13 +469,115 @@ let _4: game.LedSprite = null
 let _3: game.LedSprite = null
 let _2: game.LedSprite = null
 let _1: game.LedSprite = null
-basic.showLeds(`
-    . # # # .
-    . . . # .
-    . # # # .
-    . . . # .
-    . # # # .
-    `)
+for (let index = 0; index < 4; index++) {
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        # . . # .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . . . .
+        . . . # .
+        # . . . .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # #
+        . . . . .
+        . # . . .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . # . . .
+        . . # . .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . . . # .
+        . . . . .
+        . . . . #
+        . . . . .
+        . # # . .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . # . . .
+        . . # . .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # #
+        . . . . .
+        . . . # .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . . . .
+        . . . # .
+        . . . . #
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . # .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . # .
+        `)
+    if (gamez == 2) {
+        break;
+    }
+}
+control.raiseEvent(
+EventBusSource.MICROBIT_ID_BUTTON_B,
+EventBusValue.MICROBIT_BUTTON_EVT_DOWN
+)
 basic.forever(function () {
 	
 })
